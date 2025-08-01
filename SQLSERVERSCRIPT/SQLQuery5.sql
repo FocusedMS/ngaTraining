37b31ec8b5bd6cd4372523ba795d5d8400806331
FocusedMS <madhusudhanchittibabu@gmail.com>
2025-08-01 15:12:12 +0530

    updated

---
+++ SQLSERVERSCRIPT/SQLQuery5.sql
@@ -0,0 +1,19 @@
+select sum(basic) from Emp 
+GO
+
+select avg(basic) from Emp 
+GO
+
+select max(basic) from Emp 
+GO
+
+
+select min(basic) from Emp 
+GO
+
+select count(*) from Emp 
+GO
+
+
+select count(basic) from Emp
+Go
