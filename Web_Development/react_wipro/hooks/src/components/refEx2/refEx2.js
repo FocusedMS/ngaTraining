fe5197078334690655193a07d6b68c784947b5c9
FocusedMS <madhusudhanchittibabu@gmail.com>
2025-08-14 13:38:00 +0530

    hooks

---
+++ Web_Development/react_wipro/hooks/src/components/refEx2/refEx2.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+import './refEx2.css'
+
+export default class refEx2 extends Component {
+    render() {
+      return <div className="component-ref-ex2">Hello! component refEx2</div>;
+    }
+}
